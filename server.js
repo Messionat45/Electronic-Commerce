0955c7bc715d64c9b4f064aef7ade256bda14016
Madhukar B <tejasbondre27@gmail.com>
2025-06-14 12:09:11 +0530

    initiated products  routes

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
-const ecomRoutes = require("./routes/ecomRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
 
 const app = express();
 const port = 5000;
 
 app.use(express.json());
-app.use(ecomRoutes);
+app.use(categoryRoutes);
 
 connectDB();
 app.listen(port, () => console.log(`server started at port : ${port}`));
